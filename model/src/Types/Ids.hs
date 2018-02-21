5e48310e6462c328ee50aaca812e4318a463a494
David Hewson <dev@daveid.co.uk>
2018-02-21 19:17:10 +0000

    don't need uuid

--- model/src/Types/Ids.hs
+++ model/src/Types/Ids.hs
@@ -1,9 +1,8 @@
 module Types.Ids where
 
 import Data.Text (Text)
-import Data.UUID (UUID, fromText)
 
-newtype MyId = MyId UUID
+newtype MyId = MyId Int
 
 idFromText :: Text -> Maybe MyId
-idFromText t = MyId <$> fromText t
+idFromText _ = MyId <$> Just 0
